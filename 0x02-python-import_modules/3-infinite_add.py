bbc2b4f2be3268b0e17150f18cff2e6d57fd7b27
ZaneleYenziwe <zaneleyenziwe19@gmail.com>
2024-01-09 17:31:35 -0800

    a program that prints the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-print the result of the addition of all arguments
-def results (argv):
+def results(argv):
+    mx = len(argv) - 1
+    if mx == 0:
+        print("{:d}".format(mx))
+        return
+    else:
+        y = 1
+        res = 0
+        while y <= mx:
+            res += int(argv[y])
+            y += 1
+        print("{:d}".format(res))
 
+
+if __name__ == "__main__":
+    import sys
+    results(sys.argv)
