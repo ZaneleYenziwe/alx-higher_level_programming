12d890bb9df723a5a34e4b9af2294df3b8db05d8
ZaneleYenziwe <zaneleyenziwe19@gmail.com>
2024-01-10 16:14:00 -0800

    a function that computes the square value of all integers of a matrix

---
+++ alx-higher_level_programming/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+square_matrix_simple = _import_('0-square_matrix_simple').square_matrix_simple
+def square_matrix_simple(matrix=[]):
+  new_matrix = []
+  for row in matrix:
+    new-row = []
+    for element in row:
+
+new_row.append(element **2)
+
+ new-matrix.append(new-row)
+ 
+ return new_matrix
